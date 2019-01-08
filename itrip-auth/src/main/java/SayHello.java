cfa82ac2c258b92649f28f80f243877950b8672e
wswlw120 <464513787@qq.com>
2019-01-08 11:02:39 +0800

    Initial commit

---
+++ itrip-auth/src/main/java/SayHello.java
@@ -0,0 +1,11 @@
+public class SayHello {
+    public static final int num=10;
+
+    public static void main(String[] args) {
+
+        System.out.println("num ="+num);
+    }
+    public void testNum(){
+
+    }
+}
---
+++ itrip-biz/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World!niasa</h2>
+</body>
+</html>
